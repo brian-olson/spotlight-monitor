5a7f85fb11939ae23ef21e77185b960551704d17
Brian Olson <brian@hurrikane.net>
2022-07-19 17:23:19 +0800

    Added some debugging, changed threashold to variable (kill_percent).

--- spotlight_mem.py
+++ spotlight_mem.py
@@ -2,23 +2,25 @@
 from lib2to3.pgen2.token import PERCENT
 import psutil, os
 
+kill_percent = 5
+
 pointer = []
 for process in psutil.process_iter():
     # Get the Logitech Presentation PID.
     if process.name() == 'Logitech Presentation':
-        print("The Logitech Presentation process is", process.pid)
         pointer.append(process)
 
 for pid in pointer:
-    for _ in range(100):
-        # check memory usage over 5% physical memory
-        if pid.memory_percent() < 5 or not pid.is_running():
-            # no action, spotlight is using less than 10% of memory or process isn't running
-            print("There is no action necessary, memory utiliztion is", round(pid.memory_percent(),2),"%")
+    for _ in range(1):
+        # check memory usage over kill_percent (variable above) physical memory
+        if pid.memory_percent() < kill_percent or not pid.is_running():
+#            print("the pid is:", pid.pid ,"and the pid memory is:", pid.memory_percent(), "and the kill_percent is:", kill_percent )
+             # no action, spotlight is using less than kill_percent (variable above) of memory or process isn't running
+#            print("There is no action necessary, memory utiliztion is", round(pid.memory_percent(),2),"%")
             break
     else:
-        # spotlight is using more than 10% of physical memory and will be killed/restarted
+        # spotlight is using more than kill_percent (variable above) of physical memory and will be killed/restarted
         pid.kill()
-        print("The process is using far too much memory, KILL IT, utiliztion is", round(pid.memory_percent(),2),"%")
+#        print("The process is using far too much memory, KILL IT, utiliztion is", round(pid.memory_percent(),2),"%")
         # Execute LogiPresentation 
         os.system("/Library/Application\ Support/Logitech.localized/Logitech\ Presentation.localized/Logitech\ Presentation.app/Contents/MacOS/Logitech\ Presentation &")
